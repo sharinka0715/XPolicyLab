e6890c4f66de221656afa578d03904eb93e18f72
Tianxing Chen (陈天行) <88101805+TianxingChen@users.noreply.github.com>
2026-07-07 15:51:58 +0800

    Delete policy/Motus directory

--- policy/Motus/motus/configs/__init__.py
+++
@@ -1,1 +0,0 @@
-# Configs package for Motus
--- policy/Motus/motus/process_data.sh
+++
@@ -1,8 +0,0 @@
-export CUDA_VISIBLE_DEVICES=4
-
-python data/lerobot/add_t5_cache_to_lerobot_dataset.py \
-	--repo_id sim_stack_bowls_v21 \
-	--root "${MOTUS_DATASET_ROOT:-/path/to/lerobot/sim_stack_bowls_v21}" \
-	--wan_path "${MOTUS_WAN_PATH:-/path/to/model_weights/}" \
-	--device cuda \
-	--t5_folder_name t5_embedding
